eb318391ef33713235ff6cf5a63d0913d3debfff
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-07-01 21:50:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ main.remove();
 let newHeader = document.createElement('h1')
 // newHeader.id = 'victory'
 newHeader.setAttribute("id","victory")
+
+let newHeader = document.getElementById('h1#victory');
+header.innerHTML = "Mariel is the champion";
